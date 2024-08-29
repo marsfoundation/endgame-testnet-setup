4d5d9ba84868deb805f75e31971af3250e756dfb
Kris Kaczor <chris@kaczor.io>
2024-08-29 20:34:53 +0200

    Tests scaffolding (#9)
    * Tests scaffolding
    
    * Add basic test to test_swap_usdc
    
    * Setup CI
    
    * Small tweaks for CI

---
+++ test/SetupAllMainnet.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import { ScriptTools }       from "lib/dss-test/src/ScriptTools.sol";
+import { MainnetController } from "lib/spark-alm-controller/src/MainnetController.sol";
+import { IERC20 }            from "lib/forge-std/src/interfaces/IERC20.sol";
+import { Usds }              from "lib/usds/src/Usds.sol";
+import { ALMProxy }          from "lib/spark-alm-controller/src/ALMProxy.sol";
+
+
+contract SetupAllMainetTest is Test {
+    MainnetController mainnetController;
+    ALMProxy almProxy;
+    Usds usds;
+    address safe;
+
+    function setUp() public {
+        vm.setEnv("FOUNDRY_ROOT_CHAINID", "1");
+
+        vm.createSelectFork(vm.envString("MAINNET_RPC_URL"));
+        string memory output = ScriptTools.readOutput("mainnet");
+        
+        mainnetController = MainnetController(stdJson.readAddress(output, ".almController"));
+        almProxy = ALMProxy(stdJson.readAddress(output, ".almProxy"));
+        usds = Usds(stdJson.readAddress(output, ".usds"));
+        safe = stdJson.readAddress(output, ".safe");
+    }
+
+    function test_permissions() view public {
+        assertTrue(mainnetController.hasRole(mainnetController.RELAYER(), safe));
+    }
+
+    function test_swap_usdc() public {
+        IERC20 usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+        address usdc_whale = 0x4B16c5dE96EB2117bBE5fd171E4d203624B014aa;
+        uint256 usdcValue = 1_000_000;
+        uint256 expectedUsdsValue = 1e18;
+
+        vm.prank(usdc_whale);
+        usdc.transfer(address(almProxy), usdcValue);
+
+        vm.prank(safe);
+        mainnetController.swapUSDCToUSDS(usdcValue);
+
+        assertEq(usdc.balanceOf(address(almProxy)), 0);
+        assertEq(usds.balanceOf(address(almProxy)), expectedUsdsValue);
+    }
+}
