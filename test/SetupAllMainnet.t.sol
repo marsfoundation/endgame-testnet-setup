be8ad7f6a39fa4bd540becd8250b36170a9b210a
Kris Kaczor <chris@kaczor.io>
2024-09-02 14:34:54 +0200

    Test vat interactions (#11)
    * test vat interactions
    
    * Fix tests
    
    * Mint usdc on demand instead of relying on a whale address
    
    * Review fixes

--- test/SetupAllMainnet.t.sol
+++ test/SetupAllMainnet.t.sol
@@ -7,12 +7,14 @@ import { MainnetController } from "lib/spark-alm-controller/src/MainnetControlle
 import { IERC20 }            from "lib/forge-std/src/interfaces/IERC20.sol";
 import { Usds }              from "lib/usds/src/Usds.sol";
 import { ALMProxy }          from "lib/spark-alm-controller/src/ALMProxy.sol";
+import { Ethereum } from "lib/spark-address-registry/src/Ethereum.sol";
 
 
 contract SetupAllMainetTest is Test {
     MainnetController mainnetController;
     ALMProxy almProxy;
     Usds usds;
+    IERC20 usdc;
     address safe;
 
     function setUp() public {
@@ -25,6 +27,11 @@ contract SetupAllMainetTest is Test {
         almProxy = ALMProxy(stdJson.readAddress(output, ".almProxy"));
         usds = Usds(stdJson.readAddress(output, ".usds"));
         safe = stdJson.readAddress(output, ".safe");
+        usdc = IERC20(Ethereum.USDC);
+
+        assertEq(usds.balanceOf(address(almProxy)), 0);
+        assertEq(usdc.balanceOf(address(almProxy)), 0);
+
     }
 
     function test_permissions() view public {
@@ -32,18 +39,35 @@ contract SetupAllMainetTest is Test {
     }
 
     function test_swap_usdc() public {
-        IERC20 usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-        address usdc_whale = 0x4B16c5dE96EB2117bBE5fd171E4d203624B014aa;
         uint256 usdcValue = 1_000_000;
         uint256 expectedUsdsValue = 1e18;
 
-        vm.prank(usdc_whale);
-        usdc.transfer(address(almProxy), usdcValue);
+        deal(address(usdc), address(almProxy), usdcValue);
 
         vm.prank(safe);
         mainnetController.swapUSDCToUSDS(usdcValue);
 
         assertEq(usdc.balanceOf(address(almProxy)), 0);
         assertEq(usds.balanceOf(address(almProxy)), expectedUsdsValue);
+    }
+
+    function test_mintUSDS() public {
+        uint256 usdsValue = 1e18;
+
+        vm.prank(safe);
+        mainnetController.mintUSDS(usdsValue);
+
+        assertEq(usds.balanceOf(address(almProxy)), usdsValue);
+    }
+
+    function test_burn_usds() public {
+        uint256 usdsValue = 1e18;
+        vm.prank(safe);
+        mainnetController.mintUSDS(usdsValue);
+        
+        vm.prank(safe);
+        mainnetController.burnUSDS(usdsValue);
+
+        assertEq(usds.balanceOf(address(almProxy)), 0);
     }
 }
